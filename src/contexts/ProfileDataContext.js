1d516070e0df7ee8c6854ffa4542bb97c3ebe397
GeorgeHazaka <hazaka.george@gmail.com>
2024-01-08 21:18:09 +0000

    Allow users to edit their profile and credentials

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -8,7 +8,6 @@ export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
   const [profileData, setProfileData] = useState({
-    // we will use the pageProfile later!
     pageProfile: { results: [] },
   });
 
